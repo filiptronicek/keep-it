50709a6a7d0c1a488319f41f042055ed13d29708
Filip Troníček <filip.tronicek@seznam.cz>
2019-12-08 17:05:16 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log(`Using node.js version ${process.version}`);
+
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+var port = 4000;
+
+app.get("/", function(req, res) {
+  console.log("Rendered /src/index.html");
+  res.sendFile(path.join(__dirname + "/src/index.html"));
+});
+
+app.listen(port, function() {
+  console.log("Server is running on port:", port);
+});
